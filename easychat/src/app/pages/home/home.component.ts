ef672e4d872ea98f1bd88acdfe39d4cf1dba7c72
Andy NaZag <andynava57@gmail.com>
2023-03-12 00:39:39 -0500

    feat: getStarted btn

--- easychat/src/app/pages/home/home.component.ts
+++ easychat/src/app/pages/home/home.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
+  constructor(private router: Router) {}
 
+  onGetStarted() {
+    this.router.navigateByUrl('sign-up');
+  }
 }
